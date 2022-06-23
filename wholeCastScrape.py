e1e8172f4631c862547544b7fd7617c3c5d46dcc
Kara Christensen <30764249+christensen-kara@users.noreply.github.com>
2022-06-23 14:54:36 -0700

    Deal with blood vs water seasons
    Also removed last instance of Jeff Probst

--- wholeCastScrape.py
+++ wholeCastScrape.py
@@ -16,31 +16,92 @@ numSeasons = len(html.find('li', class_ = 'pv-h').find('ul').findAll('li'))
 # allCast is a list of lists, each list holding the cast members name, season number, and a link to their bio
 allCast = []
 
+
+# Seasons that are currently problems: 1 - 26 (only first names) and 27 and 29 (blood vs water season)
+
 for season in range(1, numSeasons + 1):
 
 	seasonUrl = url + 'season/' + str(season) + '/'
 
-	seasonData = requests.get(url)
-	seasonHtml = BeautifulSoup(data.text, 'html.parser')
+	seasonData = requests.get(seasonUrl)
+	seasonHtml = BeautifulSoup(seasonData.text, 'html.parser')
 
 	castList = seasonHtml.find('div', class_ = 'grid-view-container').findAll('a')
 
 	for person in castList:
-		# Create the list for each person to append to the main list
-		entry = []
+		# Make sure its not Jeff Probst
+		name = str.strip(person.find('div', class_ = 'title').contents[0])
 
-		# Get the persons name and add to list
-		entry.append(str.strip(person.find('span').contents[0]))
+		if 'Jeff Probst' not in name:
+			if ' & ' in name:
+				names = name.split(' & ')
 
-		# Add season number to list
-		entry.append(season)
+				for name in names:
+					# Create the list for each person to append to the main list
+					entry = []
 
-		# Add url to list
-		entry.append(baseURL + person['href'])
+					# Get the persons name and add to list
+					entry.append(name)
 
-		allCast.append(entry)
+					# Add season number to list
+					entry.append(season)
+
+					# Add url to list
+					bioURL = baseURL + person['href']
+
+					# bioData = requests.get(bioURL)
+					# bioHtml = BeautifulSoup(bioData.text, 'html.parser')
+
+					# Add url to list
+					entry.append(bioURL)
+
+					allCast.append(entry)
+
+			elif ' and ' in name:
+				names = name.split(' and ')
+
+				for name in names:
+					# Create the list for each person to append to the main list
+					entry = []
+
+					# Get the persons name and add to list
+					entry.append(name)
+
+					# Add season number to list
+					entry.append(season)
+
+					# Add url to list
+					bioURL = baseURL + person['href']
+
+					# bioData = requests.get(bioURL)
+					# bioHtml = BeautifulSoup(bioData.text, 'html.parser')
+
+					# Add url to list
+					entry.append(bioURL)
+
+					allCast.append(entry)
+
+			else:
+				# Create the list for each person to append to the main list
+				entry = []
+
+				# Get the persons name and add to list
+				entry.append(name)
+
+				# Add season number to list
+				entry.append(season)
+
+				# Add url to list
+				bioURL = baseURL + person['href']
+
+				# bioData = requests.get(bioURL)
+				# bioHtml = BeautifulSoup(bioData.text, 'html.parser')
+
+				# Add url to list
+				entry.append(bioURL)
+
+				allCast.append(entry)
 
 
 df = pd.DataFrame(allCast, columns=['Name', 'Season Number', 'Link to Bio'])
- 
 df.to_csv('nameSeasonBio.csv', index=False)
